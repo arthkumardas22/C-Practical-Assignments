27c494c575a9f4a935d49676336c190be8912937
Arth Kumar Das <arthkumardas7@gmail.com>
2026-01-03 11:09:09 +0530

    Add files via upload

---
+++ C-Practical-Assignment/Assignment-5/String_Word_Count.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    char str[200];
+    int i, count = 1;
+
+    printf("Enter a sentence:\n");
+    fgets(str, sizeof(str), stdin);
+
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ' ' && str[i + 1] != ' ')
+            count++;
+    }
+
+    printf("Number of words = %d\n", count);
+
+    return 0;
+}
